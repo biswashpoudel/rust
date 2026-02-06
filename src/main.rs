47a360532d1102e3eda234440e971aaf94450191
Biswash Poudel <poudelbiswash27@gmail.com>
2026-02-05 23:08:41 -0500

    user emailfix push

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 fn main(){
-
-    //tuple lets us to store the numeric data types together
-    //i32 stores the signed integers
-    // u32 stores the unsigned int which are always +ve
-    // f64 stores the floating points
-    let tup: (u32, i32, f64) = (20, -98, 98.76); 
-
-    let first_value: u32 = tup.0; 
-    let second_value: i32 = tup.1; 
-    let third_value: f64 = tup.2; 
-
-    println!("{}", first_value); 
-    println!("{}", second_value); 
-    println!("{}", third_value); 
+   let tuple: (i32, u32, f64) = (-20, 40, 67.50); 
+   let first_value: i32 = tuple.0; 
+   println!("{}", first_value);  
 }
