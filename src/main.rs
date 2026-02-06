66bbae78096aac43acdbcfaff1fb0b54b67571e0
Biswash Poudel <biswashpoudel@Biswashs-MacBook-Air.local>
2026-02-05 20:56:18 -0500

    tuple commit

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
-// writing the tuple syntax in rust 
+fn main(){
 
-//entry point of the rust function 
+    //tuple lets us to store the numeric data types together
+    //i32 stores the signed integers
+    // u32 stores the unsigned int which are always +ve
+    // f64 stores the floating points
+    let tup: (u32, i32, f64) = (20, -98, 98.76); 
 
-//tuple lets us to store a group of different numeric data types together inside 
-//small brackets 
-fn main(){ 
-//declaring a tuple variable with three different data types
+    let first_value: u32 = tup.0; 
+    let second_value: i32 = tup.1; 
+    let third_value: f64 = tup.2; 
 
-let tup: (i32, u32, f64) = (-27, 27, 27.27); 
-
-//accessing the tuple elements like accessing the js arrays elements
-
-let first_value: i32 = tup.0;
-
-let second_value: u32 = tup.1; 
-
-let third_value: f64 = tup.2; 
-
-//printing the values into the command line
-
-println!("{}", first_value); //should print the negative 27
-
-println!("{}", second_value); //ashould return the positive 27
-
-println!("{}", third_value); //should return the floating value 27.27
-
+    println!("{}", first_value); 
+    println!("{}", second_value); 
+    println!("{}", third_value); 
 }
